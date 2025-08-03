1aefafe2d80b90fc85e0e939e2e74b2d1db2628d
David Robert-Ansart <david.robertansart@gmail.com>
2025-08-03 12:05:03 +0200

    Add village selector

--- src/app/(1village)/Header.tsx
+++ src/app/(1village)/Header.tsx
@@ -6,12 +6,18 @@ import React, { useContext, useState } from 'react';
 import { NavigationMobileMenu } from './Navigation';
 import styles from './header.module.css';
 import { BackDrop } from '@/components/layout/BackDrop';
+import { Button } from '@/components/layout/Button';
 import { IconButton } from '@/components/layout/Button';
 import { Dropdown } from '@/components/layout/Dropdown';
 import { DropdownMenuItem } from '@/components/layout/Dropdown/DropdownMenuItem';
-import { Flex } from '@/components/layout/Flex';
+import { FlexItem } from '@/components/layout/Flex';
+import { Field } from '@/components/layout/Form';
+import { Select } from '@/components/layout/Form/Select';
+import { Modal } from '@/components/layout/Modal';
 import { UserContext } from '@/contexts/userContext';
+import { VillageContext } from '@/contexts/villageContext';
 import { logout } from '@/server-actions/authentication/logout';
+import { setVillage } from '@/server-actions/set-village';
 import CogIcon from '@/svg/cogIcon.svg';
 import LogoSVG from '@/svg/logo.svg';
 
@@ -30,10 +36,13 @@ export const Header = () => {
                         onClick={() => setIsOpen(!isOpen)}
                     />
                 </div>
-                <Flex alignItems="center">
-                    <LogoSVG className={styles.logo} />
-                    <span className={styles.title}>1Village</span>
-                </Flex>
+                <FlexItem flexGrow={1} flexShrink={1} flexBasis="0">
+                    <div className={styles.logoContainer}>
+                        <LogoSVG className={styles.logo} />
+                        <span className={styles.title}>1Village</span>
+                    </div>
+                </FlexItem>
+                {user.role === 'admin' && <VillageSelector />}
                 <Dropdown trigger={<IconButton icon={CogIcon} variant="borderless" size="lg" isTabletUpOnly />} align="end">
                     {user?.role === 'admin' && <DropdownMenuItem label="Admin" href="/admin" />}
                     <DropdownMenuItem label="Mon compte" />
@@ -46,5 +55,57 @@ export const Header = () => {
                 </BackDrop>
             )}
         </div>
+    );
+};
+
+const VillageSelector = () => {
+    const { village } = useContext(VillageContext);
+    const [isModalOpen, setIsModalOpen] = useState(village === undefined);
+    const [villageId, setVillageId] = useState(village?.id);
+    return (
+        <>
+            <Button size="sm" isUpperCase={false} color="secondary" onClick={() => setIsModalOpen(true)} label="Changer de village" />
+            <Modal
+                isOpen={isModalOpen}
+                onClose={() => {
+                    if (village !== undefined) {
+                        setIsModalOpen(false);
+                    }
+                }}
+                hasCloseButton={village !== undefined}
+                hasCancelButton={false}
+                onConfirm={async () => {
+                    if (!villageId) {
+                        return;
+                    }
+                    try {
+                        await setVillage(villageId);
+                    } catch {
+                        // TODO: handle error
+                    }
+                    setIsModalOpen(false);
+                }}
+                confirmLabel="Choisir"
+                isConfirmDisabled={!villageId}
+                title="Sélectionner un village"
+            >
+                <Field
+                    name="village"
+                    label="Village"
+                    input={
+                        <Select
+                            isFullWidth
+                            value={villageId ? `${villageId}` : undefined}
+                            onChange={(value) => setVillageId(Number(value))}
+                            placeholder="Sélectionner un village"
+                            options={[
+                                { label: 'Village 1', value: '1' },
+                                { label: 'Village 2', value: '2' },
+                            ]}
+                        />
+                    }
+                />
+            </Modal>
+        </>
     );
 };
