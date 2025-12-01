d604ea5cc7d644597f64ef69eb52f17208f8dd4a
Stephane Blanchon <stephane.blanchon@viseo.com>
2025-12-01 16:00:28 +0100

    add page.tsx + page.module.css + composant CGU in new /(1village)/cgu route

---
+++ src/app/(1village)/cgu/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { CGU } from './CGU';
+import styles from './page.module.css';
+
+const CGUPage = () => {
+    return (
+        <div className={styles.bgCgu}>
+            <CGU />
+        </div>
+    );
+};
+
+export default CGUPage;
