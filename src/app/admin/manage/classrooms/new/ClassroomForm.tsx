5ad465adf62c61531194dae3e116f92f24432df2
Benjamin HEMBERT <benjamin.hembert@orange.com>
2025-12-09 10:55:24 +0100

    feat(VIL-865): Admin: create classroom

--- src/app/admin/manage/classrooms/new/ClassroomForm.tsx
+++ src/app/admin/manage/classrooms/new/ClassroomForm.tsx
@@ -4,16 +4,22 @@ import { Button } from '@frontend/components/ui/Button';
 import { Field, Input } from '@frontend/components/ui/Form';
 import { CountrySelect } from '@frontend/components/ui/Form/CountrySelect';
 import { Select } from '@frontend/components/ui/Form/Select';
+import { Loader } from '@frontend/components/ui/Loader';
+import { COUNTRIES } from '@lib/iso-3166-countries-french';
 import { jsonFetcher } from '@lib/json-fetcher';
 import { serializeToQueryUrl } from '@lib/serialize-to-query-url';
 import type { Village } from '@server/database/schemas/villages';
+import { createClassroom } from '@server-actions/classrooms/create-classroom';
 import type { User } from 'better-auth';
-import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
 import useSWR from 'swr';
 
-import styles from './classroom-form.module.css';
+import styles from '../../manage.module.css';
 
 export function ClassroomForm() {
+    const router = useRouter();
+
     const [level, setLevel] = useState('');
     const [schoolName, setSchoolName] = useState('');
     const [address, setAddress] = useState('');
@@ -22,15 +28,62 @@ export function ClassroomForm() {
     const [village, setVillage] = useState('');
     const [teacher, setTeacher] = useState('');
 
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
+
     const { data: villages } = useSWR<Village[]>('/api/villages', jsonFetcher);
+    const { data: currentVillageResponse } = useSWR<Village[]>(
+        village ? `/api/villages${serializeToQueryUrl({ villageId: village })}` : null,
+        jsonFetcher,
+    );
     const { data: teachers } = useSWR<User[]>(`/api/users${serializeToQueryUrl({ role: 'teacher' })}`, jsonFetcher);
 
+    const currentVillage = currentVillageResponse?.[0];
+
+    const hasValidationErrors = !country || !village || !teacher || !schoolName || !address || !city;
+    const isDisabled = hasValidationErrors || isLoading;
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (hasValidationErrors) {
+            return;
+        }
+
+        setError('');
+        setIsLoading(true);
+
+        try {
+            await createClassroom({
+                level,
+                schoolName,
+                address,
+                city,
+                country,
+                villageId: Number(village),
+                teacherId: teacher,
+            });
+            router.push('/admin/manage/classrooms');
+        } catch (e) {
+            console.error(e);
+            const error = e as Error;
+            if (error.name === 'MaxClassroomsError') {
+                setError(`Le village a atteint le nombre maximum de classes pour le pays ${COUNTRIES[country]}`);
+            } else {
+                setError('Une erreur est survenue lors de la création de la classe');
+            }
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit} className={styles.form}>
+            <Loader isLoading={isLoading} />
+            {error && <div className={styles.error}>{error}</div>}
             <Field
                 label="Niveau de classe"
                 name="level"
-                marginBottom="md"
                 input={
                     <Input
                         id="level"
@@ -46,11 +99,11 @@ export function ClassroomForm() {
             <Field
                 label="Nom de l'école"
                 name="schoolName"
-                marginBottom="md"
+                isRequired
                 input={
                     <Input
-                        id="schoolName"
-                        name="schoolName"
+                        id="address"
+                        name="address"
                         color="secondary"
                         isFullWidth
                         value={schoolName}
@@ -62,7 +115,7 @@ export function ClassroomForm() {
             <Field
                 label="Adresse"
                 name="address"
-                marginBottom="md"
+                isRequired
                 input={
                     <Input
                         id="address"
@@ -78,7 +131,7 @@ export function ClassroomForm() {
             <Field
                 label="Ville"
                 name="city"
-                marginBottom="md"
+                isRequired
                 input={
                     <Input id="city" name="city" color="secondary" isFullWidth value={city} onChange={(e) => setCity(e.target.value)} type="text" />
                 }
@@ -86,7 +139,6 @@ export function ClassroomForm() {
             <Field
                 label="Pays"
                 name="country"
-                marginBottom="md"
                 isRequired
                 input={
                     <CountrySelect
@@ -95,6 +147,12 @@ export function ClassroomForm() {
                         color="secondary"
                         isFullWidth
                         value={country}
+                        filter={(country: string) => {
+                            if (currentVillage?.countries) {
+                                return currentVillage?.countries.includes(country) ?? false;
+                            }
+                            return true;
+                        }}
                         onChange={(country) => setCountry(country)}
                     />
                 }
@@ -102,7 +160,6 @@ export function ClassroomForm() {
             <Field
                 label="Village-monde"
                 name="village"
-                marginBottom="md"
                 isRequired
                 input={
                     <Select
@@ -119,7 +176,6 @@ export function ClassroomForm() {
             <Field
                 label="Professeur"
                 name="teacher"
-                marginBottom="md"
                 isRequired
                 input={
                     <Select
@@ -134,8 +190,8 @@ export function ClassroomForm() {
                 }
             />
             <div className={styles.buttons}>
-                <Button label="Annuler" variant="outlined" color="grey" type="button" />
-                <Button label="Enregistrer" variant="contained" color="secondary" type="submit" />
+                <Button label="Annuler" variant="outlined" color="grey" type="button" as="a" href="/admin/manage/classrooms" />
+                <Button label="Enregistrer" variant="contained" color="secondary" type="submit" disabled={isDisabled} />
             </div>
         </form>
     );
