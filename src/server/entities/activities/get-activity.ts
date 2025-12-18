1de8500e37315c4134e05437b582d48c751b04ab
Serge Arnone <serge.arnone@orange.com>
2025-12-18 12:12:28 +0100

    VIL-877: fix union types

--- src/server/entities/activities/get-activity.ts
+++ src/server/entities/activities/get-activity.ts
@@ -1,10 +1,42 @@
+'use server';
+
 import { db } from '@server/database';
+import type { Activity } from '@server/database/schemas/activities';
 import { activities } from '@server/database/schemas/activities';
+import type { FreeActivity, GameActivity, PuzzleActivity } from '@server/database/schemas/activity-types';
 import { eq } from 'drizzle-orm';
 
-export const getActivity = async (activityId: number) => {
+function mapToActivity(raw: Activity | null): Activity | null {
+    if (!raw) return null;
+
+    switch (raw.type) {
+        case 'libre':
+            return {
+                ...raw,
+                type: 'libre',
+                data: raw.data as FreeActivity['data'],
+            };
+        case 'jeu':
+            return {
+                ...raw,
+                type: 'jeu',
+                data: raw.data as GameActivity['data'],
+            };
+        case 'enigme':
+            return {
+                ...raw,
+                type: 'enigme',
+                data: raw.data as PuzzleActivity['data'],
+            };
+        default:
+            return null;
+    }
+}
+
+export async function getActivity(activityId: number): Promise<Activity | null> {
     const activity = await db.query.activities.findFirst({
         where: eq(activities.id, activityId),
     });
-    return activity;
-};
+
+    return mapToActivity(activity);
+}
