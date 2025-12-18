a776bdf5683a35dadf14b34473cff74fd1e6a03f
Stephane Blanchon <stephane.blanchon@viseo.com>
2025-12-18 15:45:01 +0100

    VIL-874: add listFolders() that uses listS3Folders/listLocalFolders funcs to get 'archives' subfolders + display /archives/{folder}/index.html as <Link> to archive folder

--- src/server/files/file-upload.ts
+++ src/server/files/file-upload.ts
@@ -2,8 +2,8 @@ import { getEnvVariable } from '@server/lib/get-env-variable';
 import type { Readable, Stream } from 'node:stream';
 
 import type { FileData } from './files.types';
-import { deleteLocalFile, getLocalFile, getLocalFileData, listLocalFiles, uploadLocalFile } from './local';
-import { deleteS3File, getS3File, getS3FileData, listS3Files, uploadS3File } from '../aws/s3';
+import { deleteLocalFile, getLocalFile, getLocalFileData, listLocalFiles, listLocalFolders, uploadLocalFile } from './local';
+import { deleteS3File, getS3File, getS3FileData, listS3Files, listS3Folders, uploadS3File } from '../aws/s3';
 
 export const USE_S3 = getEnvVariable('S3_BUCKET_NAME') !== '';
 
@@ -64,5 +64,13 @@ export async function listFiles(
         return {
             files: await listLocalFiles(prefix),
         };
+    }
+}
+
+export async function listFolders(prefix: string): Promise<string[]> {
+    if (USE_S3) {
+        return listS3Folders(prefix);
+    } else {
+        return listLocalFolders(prefix);
     }
 }
