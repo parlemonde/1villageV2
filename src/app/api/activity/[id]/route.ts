06f525f9442604d2f510064084a202817eff2686
Serge Arnone <serge.arnone@orange.com>
2025-12-19 16:55:39 +0100

    VIL-877: get activityId from path

--- src/app/api/activity/[id]/route.ts
+++ src/app/api/activity/[id]/route.ts
@@ -1,31 +1,19 @@
-import type { Activity } from '@server/database/schemas/activities';
 import { getActivity } from '@server/entities/activities/get-activity';
 import { getCurrentUser } from '@server/helpers/get-current-user';
 import { NextResponse, type NextRequest } from 'next/server';
-import { createLoader, parseAsInteger } from 'nuqs/server';
 
-const activitySearchParams = {
-    activityId: parseAsInteger,
-};
-const loadSearchParams = createLoader(activitySearchParams);
-
-export const GET = async ({ nextUrl }: NextRequest) => {
+export const GET = async (_req: NextRequest, { params }: { params: { id: string } }) => {
     const user = await getCurrentUser();
-    if (!user) {
-        return new NextResponse(null, { status: 401 });
-    }
+    if (!user) return new NextResponse(null, { status: 401 });
 
-    const { activityId } = loadSearchParams(nextUrl.searchParams);
+    const activityId = Number(params.id);
 
-    if (activityId === null || Number.isNaN(activityId)) {
+    if (Number.isNaN(activityId)) {
         return new NextResponse('Invalid activity ID', { status: 400 });
     }
 
-    const activity: Activity | null = await getActivity(activityId);
-
-    if (!activity) {
-        return new NextResponse('Activity not found', { status: 404 });
-    }
+    const activity = await getActivity(activityId);
+    if (!activity) return new NextResponse('Activity not found', { status: 404 });
 
     return NextResponse.json(activity);
 };
