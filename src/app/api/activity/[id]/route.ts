454cf85dcf752b65f7778b7bc58c432dbbb339b2
Serge Arnone <serge.arnone@orange.com>
2025-12-19 16:08:30 +0100

    VIL-877: check if activityId is null inside api call

--- src/app/api/activity/[id]/route.ts
+++ src/app/api/activity/[id]/route.ts
@@ -1,6 +1,6 @@
 import type { Activity } from '@server/database/schemas/activities';
 import { getActivity } from '@server/entities/activities/get-activity';
-import type { NextRequest } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
 import { createLoader, parseAsInteger } from 'nuqs/server';
 
 const activitySearchParams = {
@@ -11,15 +11,15 @@ const loadSearchParams = createLoader(activitySearchParams);
 export const GET = async ({ nextUrl }: NextRequest) => {
     const { activityId } = loadSearchParams(nextUrl.searchParams);
 
-    if (Number.isNaN(activityId)) {
-        return new Response('Invalid activity ID', { status: 400 });
+    if (activityId === null || Number.isNaN(activityId)) {
+        return new NextResponse('Invalid activity ID', { status: 400 });
     }
 
     const activity: Activity | null = await getActivity(activityId);
 
     if (!activity) {
-        return new Response('Activity not found', { status: 404 });
+        return new NextResponse('Activity not found', { status: 404 });
     }
 
-    return Response.json(activity);
+    return NextResponse.json(activity);
 };
