a19d6dfa372b4906de104641462edddfc064d4c9
Serge Arnone <serge.arnone@orange.com>
2025-12-19 16:23:07 +0100

    VIL-877: check authenticated user

--- src/app/api/activity/[id]/route.ts
+++ src/app/api/activity/[id]/route.ts
@@ -1,5 +1,6 @@
 import type { Activity } from '@server/database/schemas/activities';
 import { getActivity } from '@server/entities/activities/get-activity';
+import { getCurrentUser } from '@server/helpers/get-current-user';
 import { NextResponse, type NextRequest } from 'next/server';
 import { createLoader, parseAsInteger } from 'nuqs/server';
 
@@ -9,6 +10,11 @@ const activitySearchParams = {
 const loadSearchParams = createLoader(activitySearchParams);
 
 export const GET = async ({ nextUrl }: NextRequest) => {
+    const user = await getCurrentUser();
+    if (!user) {
+        return new NextResponse(null, { status: 401 });
+    }
+
     const { activityId } = loadSearchParams(nextUrl.searchParams);
 
     if (activityId === null || Number.isNaN(activityId)) {
