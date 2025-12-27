8b9e9c4cd6990ccb3286c09970645b77a917b7ab
David Robert-Ansart <david.robertansart@gmail.com>
2025-12-27 11:14:28 +0100

    Update stars

--- src/frontend/components/WorldMap/stars/stars.ts
+++ src/frontend/components/WorldMap/stars/stars.ts
@@ -1,4 +1,4 @@
-import { BackSide, Mesh, MeshBasicMaterial, SphereGeometry, TextureLoader, SRGBColorSpace } from 'three';
+import { BackSide, Mesh, MeshBasicMaterial, SphereGeometry, TextureLoader, SRGBColorSpace, Color } from 'three';
 
 const BACKGROUND_IMAGE_URL = '/static/images/night-sky.png';
 const SKY_RADIUS = 10;
@@ -9,12 +9,16 @@ export class Sky extends Mesh {
     constructor() {
         const skyGeometry = new SphereGeometry(SKY_RADIUS, 64, 64);
         const defaultSkyMaterial = new MeshBasicMaterial({
+            color: new Color(0, 0, 0),
             side: BackSide,
         });
         super(skyGeometry, defaultSkyMaterial);
         textureLoader.load(BACKGROUND_IMAGE_URL, (texture) => {
             texture.colorSpace = SRGBColorSpace;
+            texture.needsUpdate = true;
+            defaultSkyMaterial.color.set(1, 1, 1);
             defaultSkyMaterial.map = texture;
+            defaultSkyMaterial.needsUpdate = true;
         });
         this.name = 'sky';
     }
