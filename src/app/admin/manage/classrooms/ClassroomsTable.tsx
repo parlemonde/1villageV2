fcc8c9058581870c9be304220cdbab2dc663d48e
Benjamin Hembert <ben.hbt62@gmail.com>
2026-01-05 16:11:20 +0100

    scrollable tables

--- src/app/admin/manage/classrooms/ClassroomsTable.tsx
+++ src/app/admin/manage/classrooms/ClassroomsTable.tsx
@@ -33,7 +33,7 @@ export function ClassroomsTable() {
     });
 
     return (
-        <div>
+        <>
             <Input
                 iconAdornment={<MagnifyingGlassIcon style={{ width: '20px', height: 'auto' }} fill="currentColor" />}
                 iconAdornmentProps={{
