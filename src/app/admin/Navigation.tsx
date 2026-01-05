427d62ab6191be0703c58098a95bea02a1470a2e
Benjamin Hembert <ben.hbt62@gmail.com>
2026-01-05 13:26:23 +0100

    feat: Mobile menu for admin page + fix duplicate free content keys

--- src/app/admin/Navigation.tsx
+++ src/app/admin/Navigation.tsx
@@ -1,8 +1,12 @@
 'use client';
 
-import { Menu } from '@frontend/components/ui/Menu';
-import { BarChartIcon, GearIcon, ImageIcon, Pencil1Icon, RocketIcon } from '@radix-ui/react-icons';
+import { Menu, MobileMenu } from '@frontend/components/ui/Menu';
+import { BarChartIcon, ExitIcon, GearIcon, HomeIcon, ImageIcon, Pencil1Icon, RocketIcon } from '@radix-ui/react-icons';
+import { logout } from '@server-actions/authentication/logout';
+import classNames from 'clsx';
 import { usePathname } from 'next/navigation';
+
+import styles from './navigation.module.css';
 
 export const AdminNavigation = () => {
     const pathname = usePathname();
@@ -17,5 +21,54 @@ export const AdminNavigation = () => {
                 { label: 'Médiathèque', href: '/admin/medias', isActive: secondPath === 'medias', icon: <ImageIcon /> },
             ]}
         />
+    );
+};
+
+interface AdminMobileNavigationProps {
+    onClose: () => void;
+}
+
+export const AdminMobileNavigation = ({ onClose }: AdminMobileNavigationProps) => {
+    const pathname = usePathname();
+    const secondPath = pathname.startsWith('/admin/') ? pathname.split('/')[2] : undefined;
+
+    return (
+        <div className={styles.navigationMobileMenu} onClick={(e) => e.stopPropagation()}>
+            <div className={styles.navigationMobileMenuHeader}>
+                <div className={classNames(styles.navigationCardTitle, styles.navigationCardTitleMobile)}>
+                    <strong>Portail Admin</strong>
+                </div>
+            </div>
+            <MobileMenu
+                items={[
+                    {
+                        label: 'Créer',
+                        href: '/admin',
+                        isActive: pathname === '/admin' || secondPath === 'create',
+                        icon: <Pencil1Icon />,
+                        onClick: () => onClose(),
+                    },
+                    { label: 'Publier', href: '/admin/publish', isActive: secondPath === 'publish', icon: <RocketIcon />, onClick: () => onClose() },
+                    { label: 'Gérer', href: '/admin/manage', isActive: secondPath === 'manage', icon: <GearIcon />, onClick: () => onClose() },
+                    {
+                        label: 'Analyser',
+                        href: '/admin/analyze',
+                        isActive: secondPath === 'analyze',
+                        icon: <BarChartIcon />,
+                        onClick: () => onClose(),
+                    },
+                    { label: 'Médiathèque', href: '/admin/medias', isActive: secondPath === 'medias', icon: <ImageIcon />, onClick: () => onClose() },
+                    { label: 'Aller au village', href: '/', icon: <HomeIcon />, hasSeparatorTop: true, onClick: () => onClose() },
+                    {
+                        label: 'Se déconnecter',
+                        icon: <ExitIcon />,
+                        color: 'danger',
+                        hasSeparatorTop: true,
+                        onClick: () => logout().catch(),
+                        textAlign: 'center',
+                    },
+                ]}
+            />
+        </div>
     );
 };
