75bb1d8dae737c4cd2313fca5883108745ae91ac
Serge Arnone <serge.arnone@orange.com>
2026-01-06 11:37:32 +0100

    VIL-877: styles added + button to view the teacher's profile

--- src/app/(1village)/ActivitySidePanel.tsx
+++ src/app/(1village)/ActivitySidePanel.tsx
@@ -2,15 +2,22 @@
 
 import { WorldMap } from '@frontend/components/WorldMap';
 import { ActivityView } from '@frontend/components/activities/ActivityView';
+import { Button } from '@frontend/components/ui/Button';
+import { UserContext } from '@frontend/contexts/userContext';
 import { useParams, usePathname } from 'next/navigation';
+import { useContext } from 'react';
 import useSWR from 'swr';
 
 import styles from './activity-side-panel.module.css';
 
 export const ActivitySidePanel = () => {
+    const { user } = useContext(UserContext);
     const pathname = usePathname();
     const params = useParams();
     const activityId = Number(params?.id);
+    const formatPseudo = user.name.replace(' ', '-');
+
+    const isMediator = user?.role === 'admin' || user?.role === 'mediator';
 
     const { data: activity } = useSWR(activityId ? `/api/activity/${activityId}` : null, (url) => fetch(url).then((res) => res.json()));
 
@@ -20,7 +27,24 @@ export const ActivitySidePanel = () => {
 
     return (
         <div className={styles.activitySidePanel}>
-            {activity && <ActivityView activity={activity} showDetails={false} />}
+            <div className={styles.avatar}>{activity && <ActivityView activity={activity} showDetails={false} />}</div>
+
+            {isMediator && (
+                <div className={styles.ficheProf}>
+                    <div style={{ width: '100%', textAlign: 'center' }}>
+                        <Button
+                            as="a"
+                            href={`https://prof.parlemonde.org/les-professeurs-partenaires/${formatPseudo}/profile`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={{ overflow: 'hidden', marginBottom: '10px', textAlign: 'center' }}
+                            variant="outlined"
+                            label="Voir la fiche du professeur"
+                        ></Button>
+                    </div>
+                </div>
+            )}
+
             <div className={styles.WorldMapContainer}>
                 <WorldMap activity={activity} />
             </div>
