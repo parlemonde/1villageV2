ea4d976986ef3c7ce4e4e0782c37f718e67314c4
Serge Arnone <serge.arnone@orange.com>
2026-01-07 15:25:16 +0100

    VIL-877: fixed api/activity parameter retrieve

--- src/app/api/activity/[id]/route.ts
+++ src/app/api/activity/[id]/route.ts
@@ -2,17 +2,17 @@ import { getActivity } from '@server/entities/activities/get-activity';
 import { getCurrentUser } from '@server/helpers/get-current-user';
 import { NextResponse, type NextRequest } from 'next/server';
 
-export const GET = async (_req: NextRequest, { params }: { params: { id: string } }) => {
+export const GET = async (_req: NextRequest, { params }: { params: Promise<{ id: string }> }) => {
     const user = await getCurrentUser();
     if (!user) return new NextResponse(null, { status: 401 });
 
-    const activityId = Number(params.id);
+    const activityId = await params;
 
-    if (Number.isNaN(activityId)) {
+    if (Number.isNaN(activityId.id)) {
         return new NextResponse('Invalid activity ID', { status: 400 });
     }
 
-    const activity = await getActivity(activityId);
+    const activity = await getActivity(Number(activityId.id));
     if (!activity) return new NextResponse('Activity not found', { status: 404 });
 
     return NextResponse.json(activity);
