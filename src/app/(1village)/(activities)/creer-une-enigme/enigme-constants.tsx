bdaa06fb39ee159ce475dd7d6370efb31d1a3f5f
stephane-blanchon-viseo <stephane.blanchon@viseo.com>
2026-01-15 11:03:59 +0100

    add typing for exported useTranslatableThemes() & useTranslatableSubthemes()

--- src/app/(1village)/(activities)/creer-une-enigme/enigme-constants.tsx
+++ src/app/(1village)/(activities)/creer-une-enigme/enigme-constants.tsx
@@ -3,9 +3,22 @@ import LocalisationIcon from '@frontend/svg/enigmes/localisation-mystere.svg';
 import ObjetIcon from '@frontend/svg/enigmes/objet-mystere.svg';
 import PersonaliteIcon from '@frontend/svg/enigmes/personalite-mystere.svg';
 import { useExtracted } from 'next-intl';
-// import React from 'react';
+import React from 'react';
 
-export const useTranslatableThemes = () => {
+type ThemeName = string;
+
+type ThemeItem = {
+    name: ThemeName;
+    icon: React.ReactNode;
+};
+
+type UseTranslatableThemesReturn = {
+    DEFAULT_THEMES: ThemeItem[];
+};
+
+type UseTranslatableSubthemesReturn = { [key: ThemeName]: ThemeName[] };
+
+export const useTranslatableThemes = (): UseTranslatableThemesReturn => {
     const t = useExtracted('app.(1village).(activities).creer-une-enigme');
 
     return {
@@ -18,32 +31,15 @@ export const useTranslatableThemes = () => {
     };
 };
 
-export const useTranslatableSubthemes = () => {
+export const useTranslatableSubthemes = (subtheme: ThemeName): UseTranslatableSubthemesReturn => {
     const t = useExtracted('app.(1village).(activities).creer-une-enigme');
 
-    return {
-        DEFAULT_SUBTHEMES: {
-            [t('Objet mystère')]: [t('Un jouet'), t('Un ustentil'), t('Un objet de culte'), t('Un instrument de musique'), t('Un costume')],
-            [t('Évènement mystère')]: [t('Une fête de l’école'), t('Un festival'), t('Un fait historique'), t('Une fête nationale')],
-            [t('Personnalité mystère')]: [t('Un personnage historique'), t('Un personnage de fiction'), t('Un personnage contemporain')],
-            [t('Lieu mystère')]: [t('Un monument commémoratif'), t('Un édifice remarquable'), t('Une salle de spectacle'), t("Un ouvrage d'art")],
-        },
+    const DEFAULT_SUBTHEMES: Record<ThemeName, ThemeName[]> = {
+        t('Objet mystère'): [t('Un jouet'), t('Un ustentil'), t('Un objet de culte'), t('Un instrument de musique'), t('Un costume')],
+        t('Évènement mystère'): [t('Une fête de l’école'), t('Un festival'), t('Un fait historique'), t('Une fête nationale')],
+        t('Personnalité mystère'): [t('Un personnage historique'), t('Un personnage de fiction'), t('Un personnage contemporain')],
+        t('Lieu mystère'): [t('Un monument commémoratif'), t('Un édifice remarquable'), t('Une salle de spectacle'), t("Un ouvrage d'art")],
     };
+
+    return { subtheme: DEFAULT_SUBTHEMES[subtheme] };
 };
-
-// export const DEFAULT_THEMES: {
-//     name: string;
-//     icon: React.ReactNode;
-// }[] = [
-//     { name: 'Objet mystère', icon: <ObjetIcon /> },
-//     { name: 'Évènement mystère', icon: <EvenementIcon /> },
-//     { name: 'Personnalité mystère', icon: <PersonaliteIcon /> },
-//     { name: 'Lieu mystère', icon: <LocalisationIcon /> },
-// ];
-
-// export const DEFAULT_SUBTHEMES: Record<string, string[]> = {
-//     'Objet mystère': ['Un jouet', 'Un ustentil', 'Un objet de culte', 'Un instrument de musique', 'Un costume'],
-//     'Évènement mystère': ['Une fête de l’école', 'Un festival', 'Un fait historique', 'Une fête nationale'],
-//     'Personnalité mystère': ['Un personnage historique', 'Un personnage de fiction', 'Un personnage contemporain'],
-//     'Lieu mystère': ['Un monument commémoratif', 'Un édifice remarquable', 'Une salle de spectacle', "Un ouvrage d'art"],
-// };
