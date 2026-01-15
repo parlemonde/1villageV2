d56c6f579deb8e0087834ba914a985d7666cad01
Benjamin HEMBERT <benjamin.hembert@orange.com>
2026-01-15 14:09:52 +0100

    lint

--- src/frontend/components/activities/ActivityView/views/ReportView.tsx
+++ src/frontend/components/activities/ActivityView/views/ReportView.tsx
@@ -1,6 +1,5 @@
+import type { ActivityContentViewProps } from '@frontend/components/activities/ActivityView/activity-view.types';
 import { ContentViewer } from '@frontend/components/content/ContentViewer';
-
-import type { ActivityContentViewProps } from '../activity-view.types';
 
 export const ReportView = ({ activity }: ActivityContentViewProps) => {
     if (activity.type !== 'reportage') {
