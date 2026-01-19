dc86cc969b95aa61854700f6da39026ccb74a985
stephane-blanchon-viseo <stephane.blanchon@viseo.com>
2026-01-19 17:58:42 +0100

    VIL-886: change interface AccordionItem to allow {title,content} to be {text,className,style} objects

--- src/frontend/components/ui/Accordion/Accordion.tsx
+++ src/frontend/components/ui/Accordion/Accordion.tsx
@@ -1,55 +1,67 @@
 'use client';
 import type { MarginProps } from '@frontend/components/ui/css-styles';
-// import { getMarginAndPaddingProps, getMarginAndPaddingStyle } from '@frontend/components/ui/css-styles';
 import { ChevronDownIcon } from '@radix-ui/react-icons';
 import classNames from 'clsx';
 import { Accordion as RadixAccordion } from 'radix-ui';
 import * as React from 'react';
-import './accordion.module.css';
 
-interface AccordionItem extends MarginProps {
-    title: string | React.ReactNode;
-    content: string | React.ReactNode;
+import styles from './accordion.module.css';
+
+interface AccordionItem {
+    title: { text: string; className?: string; style?: React.CSSProperties } | React.ReactNode;
+    content: { text: string; className?: string; style?: React.CSSProperties } | React.ReactNode;
 }
 
-interface AccordionProps extends MarginProps {
+interface AccordionProps {
     items: AccordionItem[];
 }
 
-interface AccordionTriggerProps extends MarginProps {
+interface AccordionTriggerProps {
     children: React.ReactNode;
     className?: string;
+    style?: React.CSSProperties;
 }
 
-interface AccordionContentProps extends MarginProps {
+interface AccordionContentProps {
     children: React.ReactNode;
     className?: string;
+    style?: React.CSSProperties;
 }
 
 export const Accordion = ({ items }: AccordionProps) => (
-    <RadixAccordion.Root className="AccordionRoot" type="single" defaultValue="item-0" collapsible>
+    <RadixAccordion.Root className={styles.AccordionRoot} type="single" defaultValue="item-0" collapsible>
         {items.map((item, index) => (
-            <RadixAccordion.Item className="AccordionItem" value={`item-${index}`} key={`item-${index}`}>
-                <AccordionTrigger className="AccordionTrigger">{item.title}</AccordionTrigger>
-                <AccordionContent className="AccordionContent">{item.content}</AccordionContent>
+            <RadixAccordion.Item className={styles.AccordionItem} value={`item-${index}`} key={`item-${index}`}>
+                <AccordionTrigger
+                    className={`${item.title?.className ? item.title.className : ''}`}
+                    style={typeof item.title === 'object' && 'style' in item.title ? item.title.style : {}}
+                >
+                    {item.title?.text ? item.title.text : item.title}
+                </AccordionTrigger>
+                <AccordionContent
+                    className={`${item.content?.className ? item.content.className : ''}`}
+                    style={typeof item.content === 'object' && 'style' in item.content ? item.content.style : {}}
+                >
+                    {item.content?.text ? item.content.text : item.content}
+                </AccordionContent>
             </RadixAccordion.Item>
         ))}
     </RadixAccordion.Root>
 );
 
 const AccordionTrigger = React.forwardRef<HTMLButtonElement, AccordionTriggerProps>(({ children, className, ...props }, forwardedRef) => (
-    <RadixAccordion.Header className="AccordionHeader">
-        <RadixAccordion.Trigger className={classNames('AccordionTrigger', className)} {...props} ref={forwardedRef}>
+    <RadixAccordion.Header className={styles.AccordionHeader}>
+        <RadixAccordion.Trigger className={classNames(styles.AccordionTrigger, className)} {...props} ref={forwardedRef}>
             {children}
-            <ChevronDownIcon className="AccordionChevron" aria-hidden />
+            <ChevronDownIcon className={styles.AccordionChevron} aria-hidden />
         </RadixAccordion.Trigger>
     </RadixAccordion.Header>
 ));
 AccordionTrigger.displayName = 'AccordionTrigger';
 
 const AccordionContent = React.forwardRef<HTMLDivElement, AccordionContentProps>(({ children, className, ...props }, forwardedRef) => (
-    <RadixAccordion.Content className={classNames('AccordionContent', className)} {...props} ref={forwardedRef}>
-        <div className="AccordionContentText">{children}</div>
+    <RadixAccordion.Content className={classNames(styles.AccordionContent, className)} {...props} ref={forwardedRef}>
+        <div className={styles.AccordionContentText}>{children}</div>
     </RadixAccordion.Content>
 ));
 AccordionContent.displayName = 'AccordionContent';
