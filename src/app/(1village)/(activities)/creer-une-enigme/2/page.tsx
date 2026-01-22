c2d0c662f5b22fef41924febc5aec5cd0993c8ec
stephane-blanchon-viseo <stephane.blanchon@viseo.com>
2026-01-22 11:35:52 +0100

    VIL-886: use useGetStepThemeName(), fix ts errors, add previousStep buttons in creer-une-enigme/ pages

--- src/app/(1village)/(activities)/creer-une-enigme/2/page.tsx
+++ src/app/(1village)/(activities)/creer-une-enigme/2/page.tsx
@@ -1,34 +1,32 @@
 'use client';
 
-import { CUSTOM_THEME_VALUE, useEnigmeSubthemes, type ThemeName } from '@app/(1village)/(activities)/creer-une-enigme/enigme-constants';
+import { CUSTOM_THEME_VALUE, useGetStepThemeName, type ThemeName } from '@app/(1village)/(activities)/creer-une-enigme/enigme-constants';
 import { ContentEditor } from '@frontend/components/content/ContentEditor';
 import { Button } from '@frontend/components/ui/Button';
 import { PageContainer } from '@frontend/components/ui/PageContainer';
 import { Steps } from '@frontend/components/ui/Steps';
 import { Title } from '@frontend/components/ui/Title';
 import { ActivityContext } from '@frontend/contexts/activityContext';
-import { ChevronRightIcon } from '@radix-ui/react-icons';
+import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons';
 import { useContext } from 'react';
 
 export default function CreerUneEnigmeStep2() {
     const { activity, setActivity, getOrCreateDraft } = useContext(ActivityContext);
-    const DEFAULT_SUBTHEMES = useEnigmeSubthemes();
+    const defaultTheme: ThemeName = activity?.data?.defaultTheme || CUSTOM_THEME_VALUE;
+    const customTheme: string = activity?.data?.customTheme || '';
+    const stepTheme = useGetStepThemeName(defaultTheme, customTheme);
+
+    const isFirstStepDone = !!(activity?.data?.customTheme || activity?.data?.defaultTheme);
 
     if (!activity || activity.type !== 'enigme') {
         return null;
     }
 
-    const defaultTheme: ThemeName = activity.data?.defaultTheme || CUSTOM_THEME_VALUE;
-    const customTheme: string = activity.data?.customTheme || '';
-    const step1Theme = DEFAULT_SUBTHEMES[defaultTheme]?.find((subtheme) => subtheme.name === customTheme)?.tname || customTheme;
-
-    const isFirstStepDone = !!(activity.data?.customTheme || activity.data?.defaultTheme);
-
     return (
         <PageContainer>
             <Steps
                 steps={[
-                    { label: step1Theme || 'Énigme', href: '/creer-une-enigme/1', status: isFirstStepDone ? 'success' : 'warning' },
+                    { label: stepTheme || 'Énigme', href: '/creer-une-enigme/1', status: isFirstStepDone ? 'success' : 'warning' },
                     { label: "Créer l'énigme", href: '/creer-une-enigme/2' },
                     { label: 'Réponse', href: '/creer-une-enigme/3' },
                     { label: 'Pré-visualiser', href: '/creer-une-enigme/4' },
@@ -47,7 +45,15 @@ export default function CreerUneEnigmeStep2() {
                     activityId={activity.id}
                     getActivityId={getOrCreateDraft}
                 />
-                <div style={{ textAlign: 'right', marginTop: '16px' }}>
+                <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '16px' }}>
+                    <Button
+                        as="a"
+                        href="/creer-une-enigme/1"
+                        color="primary"
+                        variant="outlined"
+                        label="Étape précédente"
+                        leftIcon={<ChevronLeftIcon />}
+                    />
                     <Button as="a" href="/creer-une-enigme/3" color="primary" label="Étape suivante" rightIcon={<ChevronRightIcon />}></Button>
                 </div>
             </div>
