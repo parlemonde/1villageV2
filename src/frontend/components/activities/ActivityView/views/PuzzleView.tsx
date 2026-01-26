312526bac2412e5f49f107bed064d090881339d3
stephane-blanchon-viseo <stephane.blanchon@viseo.com>
2026-01-26 12:10:11 +0100

    VIL-886: fix eslint errors

--- src/frontend/components/activities/ActivityView/views/PuzzleView.tsx
+++ src/frontend/components/activities/ActivityView/views/PuzzleView.tsx
@@ -1,6 +1,5 @@
+import type { ActivityContentViewProps } from '@frontend/components/activities/ActivityView/activity-view.types';
 import { ContentViewer } from '@frontend/components/content/ContentViewer';
-
-import type { ActivityContentViewProps } from '../activity-view.types';
 
 export const PuzzleView = ({ activity }: ActivityContentViewProps) => {
     if (activity.type !== 'enigme') {
