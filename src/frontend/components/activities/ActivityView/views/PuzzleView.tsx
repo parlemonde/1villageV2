d0173f30944bdcd03c2a7a046b366e5c241b89bb
stephane-blanchon-viseo <stephane.blanchon@viseo.com>
2026-02-03 12:11:05 +0100

    VIL-909: add PuzzleViewTitle (defaultTheme) and puzzleViewFooter (buttons) in PuzzleView.tsx

--- src/frontend/components/activities/ActivityView/views/PuzzleView.tsx
+++ src/frontend/components/activities/ActivityView/views/PuzzleView.tsx
@@ -1,9 +1,44 @@
+import { ActivityResponseButton } from '@frontend/components/activities/ActivityTimer/ActivityTimer';
 import type { ActivityContentViewProps } from '@frontend/components/activities/ActivityView/activity-view.types';
 import { ContentViewer } from '@frontend/components/content/ContentViewer';
+import { Button } from '@frontend/components/ui/Button/Button';
+import classNames from 'clsx';
+import { useExtracted } from 'next-intl';
+import { useState } from 'react';
+
+import styles from './puzzle-view.module.css';
 
 export const PuzzleView = ({ activity }: ActivityContentViewProps) => {
+    const tCommon = useExtracted('common');
+    const [hintVisible, setHintVisible] = useState(false);
+    const themeLabel = activity?.type === 'enigme' ? activity.data?.defaultTheme : tCommon('Autre thème');
+    const hasHintValue = activity?.type === 'enigme' ? activity.data?.defaultTheme !== 'Autre thème' : false;
+
     if (activity.type !== 'enigme') {
         return null;
     }
-    return <ContentViewer content={activity.data?.content} activityId={activity.id} />;
+
+    return (
+        <>
+            <div className={classNames(styles.puzzleViewTitle)}>{themeLabel}</div>
+            <ContentViewer content={activity.data?.content} activityId={activity.id} marginTop={20} marginBottom={50} />
+            <div className={classNames(styles.puzzleViewFooter)}>
+                {hasHintValue && (
+                    <Button
+                        color="primary"
+                        variant="outlined"
+                        label={tCommon('Obtenir un autre indice')}
+                        onClick={() => setHintVisible(!hintVisible)}
+                    />
+                )}
+                <ActivityResponseButton activity={activity} />
+            </div>
+            {hintVisible && (
+                <div className={styles.hintSection}>
+                    <div className={styles.hintHeader}>indice supplémentaire</div>
+                    {tCommon("L'indice supplémentaire est {hint}", { hint: activity.data?.customTheme || '' })}
+                </div>
+            )}
+        </>
+    );
 };
--- src/frontend/components/content/ContentViewer/ContentViewer.tsx
+++ src/frontend/components/content/ContentViewer/ContentViewer.tsx
@@ -1,16 +1,18 @@
 import type { Content } from '@frontend/components/content/content.types';
-import React from 'react';
+import type { MarginProps, PaddingProps } from '@frontend/components/ui/css-styles';
+import { getMarginAndPaddingProps, getMarginAndPaddingStyle } from '@frontend/components/ui/css-styles';
 
 import { AnyContentViewer } from './AnyContentViewer';
 
-interface ContentViewerProps {
+interface ContentViewerProps extends MarginProps, PaddingProps {
     content?: Content;
     activityId?: number;
 }
 
-export const ContentViewer = ({ content = [], activityId }: ContentViewerProps) => {
+export const ContentViewer = ({ content = [], activityId, ...otherProps }: ContentViewerProps) => {
+    const { marginAndPaddingProps } = getMarginAndPaddingProps(otherProps);
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', gap: '32px', margin: '32px 0' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '32px', margin: '32px 0', ...getMarginAndPaddingStyle(marginAndPaddingProps) }}>
             {content.map((content, index) => (
                 <AnyContentViewer key={index} content={content} activityId={activityId} />
             ))}
