1a6a7215e91bb59ea10dfee83ee4c90a4a72b822
Benjamin Hembert <ben.hbt62@gmail.com>
2026-02-06 09:43:44 +0100

    Use draftUrl instead of routes map

--- src/app/(1village)/(activities)/creer-un-jeu/page.tsx
+++ src/app/(1village)/(activities)/creer-un-jeu/page.tsx
@@ -1,23 +1,22 @@
 'use client';
 
+import type { Theme } from '@frontend/components/ThemesGrid/ThemesGrid';
 import { ThemesGrid } from '@frontend/components/ThemesGrid/ThemesGrid';
 import { PageContainer } from '@frontend/components/ui/PageContainer';
 import CurrencyIcon from '@frontend/svg/game/currency.svg';
 import GestureIcon from '@frontend/svg/game/gesture.svg';
 import IdiomIcon from '@frontend/svg/game/idiom.svg';
-import { getGameFormRoutes } from '@lib/get-game-form-routes';
 import type { GameType } from '@server/database/schemas/activity-types';
 import { useRouter } from 'next/navigation';
 import { useExtracted } from 'next-intl';
-import type { ReactElement } from 'react';
 
 const useGameThemes = () => {
     const t = useExtracted('app.(1village).(activities).creer-un-jeu');
 
-    const GAME_THEMES: { name: GameType; label: string; icon: ReactElement }[] = [
-        { name: 'mimique', label: t('Jeu des mimiques'), icon: <GestureIcon /> },
-        { name: 'monnaie', label: t('Jeu de la monnaie'), icon: <CurrencyIcon /> },
-        { name: 'expression', label: t('Jeu des expressions'), icon: <IdiomIcon /> },
+    const GAME_THEMES: Theme<GameType>[] = [
+        { name: 'mimique', label: t('Jeu des mimiques'), icon: <GestureIcon />, href: '/creer-un-jeu/mimique' },
+        { name: 'monnaie', label: t('Jeu de la monnaie'), icon: <CurrencyIcon />, href: '/creer-un-jeu/monnaie' },
+        { name: 'expression', label: t('Jeu des expressions'), icon: <IdiomIcon />, href: '/creer-un-jeu/expression' },
     ];
 
     return GAME_THEMES;
@@ -33,8 +32,8 @@ export default function CreerUnJeuPage() {
         <PageContainer title={t('Choisissez le jeu que vous souhaitez créer')}>
             <ThemesGrid
                 themes={gameThemes}
-                onClick={(themeName) => {
-                    router.push(getGameFormRoutes(themeName));
+                onClick={(href) => {
+                    router.push(href);
                 }}
             />
         </PageContainer>
--- src/frontend/components/ThemesGrid/ThemesGrid.tsx
+++ src/frontend/components/ThemesGrid/ThemesGrid.tsx
@@ -6,18 +6,19 @@ export interface Theme<T extends string> {
     name: T;
     label: string;
     icon?: ReactElement;
+    href: string;
 }
 
 interface ThemesGridProps<T extends string> {
     themes: Theme<T>[];
-    onClick: (theme: T) => void;
+    onClick: (theme: string) => void;
 }
 
 export function ThemesGrid<T extends string>({ themes, onClick }: ThemesGridProps<T>) {
     return (
         <div className={styles.themesContainer}>
             {themes.map((theme) => (
-                <button key={theme.name} className={styles.themeLink} onClick={() => onClick(theme.name)}>
+                <button key={theme.name} className={styles.themeLink} onClick={() => onClick(theme.href)}>
                     {theme.icon}
                     {theme.label}
                 </button>
