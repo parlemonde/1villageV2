ad2477f2c5d5c80c56dd37dd3858f43f5ccc7ab6
Serge Arnone <serge.arnone@orange.com>
2026-02-11 10:17:32 +0100

    VIL-867: Pelico presentation display page init

--- src/app/pelico/page.tsx
+++ src/app/pelico/page.tsx
@@ -1,8 +1,15 @@
-export default function PelicoPage() {
-  return (
-    <div>
-      <h1>Page Pelico</h1>
-      <p>Contenu de votre présentation Pelico</p>
-    </div>
-  );
+import { ContentViewer } from '@frontend/components/content/ContentViewer/ContentViewer';
+import { PageContainer } from '@frontend/components/ui/PageContainer/PageContainer';
+import { Title } from '@frontend/components/ui/Title';
+import { getPelicoPresentation } from '@server-actions/activities/get-pelico-presentation';
+
+export default async function PelicoPage() {
+    const presentation = await getPelicoPresentation();
+
+    return (
+        <PageContainer>
+            <Title marginY="md">Pélico, la mascotte d&apos;1Village, se présente</Title>
+            {presentation?.data && <ContentViewer content={presentation.data.content} activityId={presentation.id} />}
+        </PageContainer>
+    );
 }
