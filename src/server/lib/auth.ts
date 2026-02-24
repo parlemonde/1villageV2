cf0dc6becca7ac03944dbdfae9780fc2de2760e8
Serge Arnone <serge.arnone@orange.com>
2026-02-24 11:17:19 +0100

    VIL-924: firstLogin column added to database schema + betterauth session + users api + user context

--- src/server/lib/auth.ts
+++ src/server/lib/auth.ts
@@ -57,6 +57,13 @@ export const auth = registerService('auth', () =>
         },
         user: {
             modelName: 'users',
+            additionalFields: {
+                firstLogin: {
+                    type: 'number',
+                    fieldName: 'firstLogin',
+                    input: false,
+                },
+            },
             changeEmail: {
                 enabled: true,
                 updateEmailWithoutVerification: true,
