e9e4bc870e7411c61f29bcd7fb7ea1b350c2c9a9
Serge Arnone <serge.arnone@orange.com>
2026-02-24 15:08:39 +0100

    VIL-924: welcome modal - step 1

--- src/frontend/components/WelcomeModal/WelcomeModal.tsx
+++ src/frontend/components/WelcomeModal/WelcomeModal.tsx
@@ -1,35 +1,35 @@
 'use client';
 
+import { Modal } from '@frontend/components/ui/Modal';
 import { UserContext } from '@frontend/contexts/userContext';
 import { VillageContext } from '@frontend/contexts/villageContext';
 import { useContext } from 'react';
 
-import { FirstPhase } from './FirstPhase';
-import { SecondPhase } from './SecondPhase';
-import { ThirdPhase } from './ThirdPhase';
-import { UnassignedVillageModal } from './UnassignedVillageModal';
+import { StepVillageName } from './StepVillageName';
+import styles from './welcome-modal.module.css';
 
 export const WelcomeModal = () => {
     const { user } = useContext(UserContext);
     const { village } = useContext(VillageContext);
 
-    if (!user || user.role !== 'teacher') {
+    if (!user || !village || user.role !== 'teacher' || user.firstLogin !== 0) {
         return null;
     }
 
-    if (!village) {
-        return <UnassignedVillageModal />;
-    }
-
-    if (user.firstLogin === 0) {
-        return <FirstPhase />;
-    }
-    if (user.firstLogin === 1 && village.activePhase > 1) {
-        return <SecondPhase />;
-    }
-    if (user.firstLogin === 2 && village.activePhase > 2) {
-        return <ThirdPhase />;
-    }
-
-    return null;
+    return (
+        <Modal
+            isOpen={true}
+            onClose={() => {}}
+            title="Bienvenue sur 1Village !"
+            hasCloseButton={false}
+            hasFooter={false}
+            width="lg"
+            isFullWidth
+            onOpenAutoFocus={false}
+        >
+            <div className={styles.stepContainer}>
+                <StepVillageName villageName={village.name} />
+            </div>
+        </Modal>
+    );
 };
