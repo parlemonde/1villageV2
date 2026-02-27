7be7a2fed2cb81228afb73c3d827bb34b6c74104
Serge Arnone <serge.arnone@orange.com>
2026-02-27 15:07:49 +0100

    VIL-924: WelcomeModal - step 4 - Teacher name & address. TODO: pick styles from V1 + right message preview panel

---
+++ src/server-actions/users/update-first-login.ts
@@ -0,0 +1,23 @@
+'use server';
+
+import { db } from '@server/database';
+import { users } from '@server/database/schemas/users';
+import { getCurrentUser } from '@server/helpers/get-current-user';
+import type { ServerActionResponse } from '@server-actions/common/server-action-response';
+import { eq } from 'drizzle-orm';
+
+export const updateFirstLogin = async (firstLogin: number): Promise<ServerActionResponse> => {
+    try {
+        const user = await getCurrentUser();
+        if (!user) {
+            throw new Error('Unauthorized');
+        }
+
+        await db.update(users).set({ firstLogin }).where(eq(users.id, user.id));
+
+        return {};
+    } catch (e) {
+        console.error(e);
+        return { error: { message: 'Une erreur est survenue lors de la mise à jour du statut de connexion' } };
+    }
+};
