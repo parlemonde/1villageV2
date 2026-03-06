7f8b1381748a7dd4bf3d9dcea40b9b743acd58ea
Serge Arnone <serge.arnone@orange.com>
2026-03-06 16:25:59 +0100

    VIL-924: wip: no village assigned modal. TODO: check styles + check if modal must be opened by VillageContext

--- src/frontend/components/WelcomeModal/WelcomeModal.tsx
+++ src/frontend/components/WelcomeModal/WelcomeModal.tsx
@@ -7,13 +7,18 @@ import { useContext } from 'react';
 import { FirstPhase } from './FirstPhase';
 import { SecondPhase } from './SecondPhase';
 import { ThirdPhase } from './ThirdPhase';
+import { UnassignedVillageModal } from './UnassignedVillageModal';
 
 export const WelcomeModal = () => {
     const { user } = useContext(UserContext);
     const { village } = useContext(VillageContext);
 
-    if (!user || !village || user.role !== 'teacher') {
+    if (!user || user.role !== 'teacher') {
         return null;
+    }
+
+    if (!village) {
+        return <UnassignedVillageModal />;
     }
 
     if (user.firstLogin === 0) {
