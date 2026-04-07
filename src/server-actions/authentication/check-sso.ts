d8c2c19d62732d5c1de85730166db432416c5e3f
stephane-blanchon-viseo <stephane.blanchon@viseo.com>
2026-04-07 17:45:12 +0200

    VIL-979: split server-action into 2 separate ones: login.ts and check-sso.ts

---
+++ src/server-actions/authentication/check-sso.ts
@@ -0,0 +1,24 @@
+/* eslint-disable camelcase */
+'use server';
+
+import { db } from '@server/database';
+import { auth_accounts } from '@server/database/schemas/auth-schemas';
+import { users } from '@server/database/schemas/users';
+import { PARLEMONDE_SSO_PROVIDER_ID } from '@server/lib/parlemonde-sso-plugin';
+import { eq, and } from 'drizzle-orm';
+
+export async function checkSSO(email: string): Promise<string> {
+    const ssoProvider = PARLEMONDE_SSO_PROVIDER_ID;
+    const result = await db
+        .select({
+            providers: auth_accounts.providerId,
+        })
+        .from(auth_accounts)
+        .leftJoin(users, eq(users.id, auth_accounts.userId))
+        .where(and(eq(users.email, email)));
+
+    if (result.some((r) => r.providers === ssoProvider) && result.every((r) => r.providers !== 'credential')) {
+        return 'Veuillez utiliser la connection par classe pour accéder à votre compte.';
+    }
+    return 'OK';
+}
--- src/server-actions/authentication/login.ts
+++ src/server-actions/authentication/login.ts
@@ -1,29 +1,19 @@
-/* eslint-disable camelcase */
 'use server';
 
-import { db } from '@server/database';
-import { auth_accounts } from '@server/database/schemas/auth-schemas';
-import { users, type User } from '@server/database/schemas/users';
+import { type User } from '@server/database/schemas/users';
 import { auth } from '@server/lib/auth';
 import { getStringValue } from '@server/lib/get-string-value';
-import { PARLEMONDE_SSO_PROVIDER_ID } from '@server/lib/parlemonde-sso-plugin';
-import { eq, and } from 'drizzle-orm';
 import { redirect, RedirectType } from 'next/navigation';
+
+import { checkSSO } from './check-sso';
 
 export async function login(_previousState: string, formData: FormData): Promise<string> {
     const email = getStringValue(formData.get('email'));
     const password = getStringValue(formData.get('password'));
+    const result = await checkSSO(email);
 
-    const ssoProvider = PARLEMONDE_SSO_PROVIDER_ID;
-    const result = await db
-        .select({
-            providers: auth_accounts.providerId,
-        })
-        .from(auth_accounts)
-        .leftJoin(users, eq(users.id, auth_accounts.userId))
-        .where(and(eq(users.email, email)));
-    if (result.some((r) => r.providers === ssoProvider) && result.every((r) => r.providers !== 'credential')) {
-        return 'Veuillez utiliser la connection par classe pour accéder à votre compte.';
+    if (result !== 'OK') {
+        return result;
     }
 
     let user: User | undefined;
