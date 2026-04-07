b107e454065ad13ecd02e0bc336a9f3eccd2cde4
stephane-blanchon-viseo <stephane.blanchon@viseo.com>
2026-04-07 17:48:06 +0200

    VIL-979: configure betterAuth.emailAndPassword's sendResetPassword() & onPasswordReset() in src/server/lib/auth.ts

--- src/server/lib/auth.ts
+++ src/server/lib/auth.ts
@@ -1,4 +1,5 @@
 import { db } from '@server/database';
+import type { EmailType } from '@server/emails/templates/utils/types';
 import { registerService } from '@server/lib/register-service';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
@@ -6,7 +7,9 @@ import { nextCookies } from 'better-auth/next-js';
 import { admin } from 'better-auth/plugins';
 import { adminAc, userAc } from 'better-auth/plugins/admin/access';
 
+import { logger } from './logger';
 import { ssoPlugin } from './parlemonde-sso-plugin';
+import { sendEmail } from './sendEmail';
 
 const adminPlugin = admin({
     defaultRole: 'teacher',
@@ -27,6 +30,23 @@ export const auth = registerService('auth', () =>
         plugins: ssoPlugin ? [ssoPlugin, adminPlugin, cookiesPlugin] : [adminPlugin, cookiesPlugin], // make sure `nextCookies()` is the last plugin in the array
         emailAndPassword: {
             enabled: true,
+            sendResetPassword: async ({ user, url, token }) => {
+                logger.info(`sendResetPassword to user ${user.email}`);
+                logger.info(`sendResetPassword containing url ${url}`, { token: token });
+                void sendEmail({
+                    to: user.email,
+                    subject: 'Reset your password',
+                    emailType: 'RESET_PASSWORD' as EmailType,
+                    props: {
+                        firstName: user.name ?? '',
+                        resetPasswordLink: url,
+                    },
+                });
+            },
+            onPasswordReset: async ({ user }, request) => {
+                // your logic here
+                logger.info(`Password for user ${user.email} has been reset.`);
+            },
         },
         advanced: {
             database: {
