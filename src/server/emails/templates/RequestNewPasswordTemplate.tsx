1cbb495ea16fa01efd481bf4d9dfcd0ef3ec8e76
stephane-blanchon-viseo <stephane.blanchon@viseo.com>
2026-04-08 15:34:40 +0200

    VIL-979: slight rework RequestNewPasswordTemplate.tsx + use plm-logo-allonge.jpg

--- src/server/emails/templates/RequestNewPasswordTemplate.tsx
+++ src/server/emails/templates/RequestNewPasswordTemplate.tsx
@@ -1,4 +1,4 @@
-import { Html, Container, Section, Row, Column, Heading, Img, Link, Text, Font } from '@react-email/components';
+import { Html, Container, Section, Heading, Img, Link, Text, Font } from '@react-email/components';
 import type { BaseTemplateProps } from '@server/emails/templates/BaseTemplate';
 import styles from '@server/emails/templates/utils/emailStyles';
 import { getEnvVariable } from '@server/lib/get-env-variable';
@@ -21,23 +21,27 @@ export default function RequestNewPasswordTemplate({
     baseTranslations,
 }: RequestNewPasswordTemplateProps) {
     const { initializeNewPassword, followLinktoResetPassword, goTo1Village, followUs } = translations;
-    const { altText } = baseTranslations;
+    const { altText, notification } = baseTranslations;
     const baseUrl = getEnvVariable('HOST_URL');
 
     return (
         <Html>
             <Font fontFamily="Roboto" fallbackFontFamily="Arial" fontWeight={400} fontStyle="normal" />
-            <Container style={{ padding: '16px', backgroundColor: styles.primaryColor, textAlign: 'center' }}>
-                <Section style={{ padding: '16px', backgroundColor: styles.backgroundColor }}>
-                    <Container style={{ backgroundColor: 'lightblue' }}>
-                        <Img src={`${baseUrl}/static/images/plm-logo.png`} width={90} height={66} alt={altText} />
+            <Container style={{ padding: '16px', backgroundColor: styles.primaryColor }}>
+                <Container style={{ textAlign: 'center', backgroundColor: styles.backgroundColor }}>
+                    <Section>
+                        <Img src={`${baseUrl}/static/images/plm-logo-allonge.jpg`} width="100%" height="auto" alt={altText} />
                         <Heading as="h2" style={{ margin: '16px 0' }}>
                             {initializeNewPassword}
                         </Heading>
-                    </Container>
-                    <Text>{followLinktoResetPassword}</Text>
-                    <Link href={resetPasswordLink}>{resetPasswordLink}</Link>
-                    <Section style={{ textAlign: 'center' }}>
+                    </Section>
+                    <Section style={{ textAlign: 'left', padding: '16px' }}>
+                        <Text style={{ margin: '0' }}>{followLinktoResetPassword}</Text>
+                        <Link style={{ fontSize: '14px' }} href={resetPasswordLink}>
+                            {resetPasswordLink}
+                        </Link>
+                    </Section>
+                    <Section style={{ padding: '16px' }}>
                         <Link
                             href={baseUrl}
                             target="_blank"
@@ -46,29 +50,22 @@ export default function RequestNewPasswordTemplate({
                             <Img src={`${baseUrl}/static/images/android-chrome-192x192.png`} alt="1Village" width={24} height={24} /> {goTo1Village}
                         </Link>
                     </Section>
-                    <Section style={{ backgroundColor: 'lightpink' }}>
-                        <Text>{followUs}</Text>
-                    </Section>
                     <Section>
-                        <Row>
-                            <Column align="right">
-                                <Link href="https://www.facebook.com/associationparlemonde" style={{ display: 'inline-block' }}>
-                                    <Img src={`${baseUrl}/static/svg/icon-facebook.svg`} alt="Facebook" />
-                                </Link>
-                            </Column>
-                            <Column align="center" width={50}>
-                                <Link href="https://www.linkedin.com/company/par-le-monde/" style={{ display: 'inline-block' }}>
-                                    <Img src={`${baseUrl}/static/svg/icon-linkedin.svg`} alt="LinkedIn" />
-                                </Link>
-                            </Column>
-                            <Column align="left">
-                                <Link href="https://www.youtube.com/@parlemonde6324" style={{ display: 'inline-block' }}>
-                                    <Img src={`${baseUrl}/static/svg/icon-youtube.svg`} alt="Youtube" />
-                                </Link>
-                            </Column>
-                        </Row>
+                        <Text style={{ fontSize: '12px', fontStyle: 'italic', color: styles.grey500 }}>{notification}</Text>
                     </Section>
-                </Section>
+                    <Section style={{ padding: '0 0 16px' }}>
+                        <Text style={{ fontSize: '16px', fontWeight: 'bold', textTransform: 'uppercase', marginBottom: '8px' }}>{followUs}</Text>
+                        <Link href="https://www.facebook.com/associationparlemonde" style={{ ...styles.socialButton, margin: '0 4px' }}>
+                            <Img src={`${baseUrl}/static/svg/icon-facebook.svg`} alt="Facebook" width={18} height={18} />
+                        </Link>
+                        <Link href="https://www.linkedin.com/company/par-le-monde/" style={{ ...styles.socialButton, margin: '0 4px' }}>
+                            <Img src={`${baseUrl}/static/svg/icon-linkedin.svg`} alt="LinkedIn" width={18} height={18} />
+                        </Link>
+                        <Link href="https://www.youtube.com/@parlemonde6324" style={{ ...styles.socialButton, margin: '0 4px' }}>
+                            <Img src={`${baseUrl}/static/svg/icon-youtube.svg`} alt="Youtube" width={18} height={18} />
+                        </Link>
+                    </Section>
+                </Container>
             </Container>
         </Html>
     );
@@ -88,7 +85,7 @@ RequestNewPasswordTemplate.PreviewProps = {
         initializeNewPassword: 'Réinitialisation de votre mot de passe',
         followLinktoResetPassword: 'En suivant le lien ci-dessous, vous pourrez modifier votre mot de passe :',
         goTo1Village: 'Allez sur 1Village',
-        followUs: 'Suivez-nous',
+        followUs: 'Suivez-nous !',
     },
 } as const;
 
@@ -99,7 +96,7 @@ export const getRequestNewPasswordTranslations = async () => {
         initializeNewPassword: t('Réinitialisation de votre mot de passe'),
         followLinktoResetPassword: t('En suivant le lien ci-dessous, vous pourrez modifier votre mot de passe :'),
         goTo1Village: t('Allez sur 1Village'),
-        followUs: t('Suivez-nous'),
+        followUs: t('Suivez-nous !'),
     };
 
     return translations;
