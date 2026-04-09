259f4100e87bfca4bd0622ffe838c336289ab7c3
stephane-blanchon-viseo <stephane.blanchon@viseo.com>
2026-04-09 12:19:56 +0200

    VIL-979: add flex container + <Title> in ResetPasswordForm.tsx

--- src/app/login/famille/reset-password/ResetPasswordForm.tsx
+++ src/app/login/famille/reset-password/ResetPasswordForm.tsx
@@ -3,6 +3,7 @@
 import { Button } from '@frontend/components/ui/Button';
 import { IconButton } from '@frontend/components/ui/Button/IconButton';
 import { Field, Input } from '@frontend/components/ui/Form';
+import { Title } from '@frontend/components/ui/Title';
 import { EyeNoneIcon, EyeOpenIcon } from '@radix-ui/react-icons';
 import { resetPassword } from '@server-actions/authentication/reset-password';
 import { useExtracted } from 'next-intl';
@@ -20,37 +21,42 @@ export const ResetPasswordForm = ({ token }: ResetPasswordFormProps) => {
     const [message, resetPasswordAction, isPending] = useActionState(resetPassword, '');
 
     return (
-        <form action={resetPasswordAction} className={styles.resetForm}>
-            {isPending && <p style={{ color: 'var(--success-color)', textAlign: 'center' }}>{t('Veuillez patienter...')}</p>}
-            {message && <p style={{ color: 'var(--error-color)', textAlign: 'center' }}>{message}</p>}
-            <Field
-                name="password"
-                label="Mot de passe"
-                input={
-                    <Input
-                        id="password"
-                        name="password"
-                        type={showPassword ? 'text' : 'password'}
-                        isFullWidth
-                        required
-                        placeholder="Entrez votre mot de passe"
-                        iconAdornment={
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={() => {
-                                    setShowPassword(!showPassword);
-                                }}
-                                variant="borderless"
-                                icon={showPassword ? EyeNoneIcon : EyeOpenIcon}
-                                iconProps={{ style: { color: 'rgba(0, 0, 0, 0.54)', height: 24, width: 24 } }}
-                            ></IconButton>
-                        }
-                        iconAdornmentProps={{ position: 'right' }}
-                    />
-                }
-            />
-            <Input type="hidden" name="token" value={token} />
-            <Button label="Se connecter" type="submit" color="primary" />
-        </form>
+        <div style={{ display: 'flex', width: '100%', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', padding: '16px' }}>
+            <Title variant="h2" color="inherit" marginY="md">
+                {t('Réinitialisation du mot de passe')}
+            </Title>
+            <form action={resetPasswordAction} className={styles.resetForm}>
+                {isPending && <p style={{ color: 'var(--success-color)', textAlign: 'center' }}>{t('Veuillez patienter...')}</p>}
+                {message && <p style={{ color: 'var(--error-color)', textAlign: 'center' }}>{message}</p>}
+                <Field
+                    name="password"
+                    label="Mot de passe"
+                    input={
+                        <Input
+                            id="password"
+                            name="password"
+                            type={showPassword ? 'text' : 'password'}
+                            isFullWidth
+                            required
+                            placeholder="Entrez votre mot de passe"
+                            iconAdornment={
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={() => {
+                                        setShowPassword(!showPassword);
+                                    }}
+                                    variant="borderless"
+                                    icon={showPassword ? EyeNoneIcon : EyeOpenIcon}
+                                    iconProps={{ style: { color: 'rgba(0, 0, 0, 0.54)', height: 24, width: 24 } }}
+                                ></IconButton>
+                            }
+                            iconAdornmentProps={{ position: 'right' }}
+                        />
+                    }
+                />
+                <Input type="hidden" name="token" value={token} />
+                <Button label="Se connecter" type="submit" color="primary" />
+            </form>
+        </div>
     );
 };
