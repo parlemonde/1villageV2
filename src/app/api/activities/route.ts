58960ef526ad782fc3aeef9471ea18ccd9aa2480
Benjamin <benjamin.hembert@orange.com>
2026-04-13 11:15:39 +0200

    fix get activities as admin

--- src/app/api/activities/route.ts
+++ src/app/api/activities/route.ts
@@ -27,10 +27,7 @@ export const GET = async ({ nextUrl }: NextRequest) => {
         return new NextResponse(null, { status: 401 });
     }
 
-    const { classroom } = await getCurrentVillageAndClassroomForUser(user);
-    if (!classroom) {
-        return new NextResponse(null, { status: 400 });
-    }
+    const { classroom: _classroom } = await getCurrentVillageAndClassroomForUser(user);
 
     const { activityId, search, phase, villageId, type, isPelico, countries } = loadSearchParams(nextUrl.searchParams);
 
@@ -57,7 +54,7 @@ export const GET = async ({ nextUrl }: NextRequest) => {
         .leftJoin(classrooms, eq(activities.classroomId, classrooms.id)) // Used to filter by countries
         .where(
             and(
-                // TODO migration classroom.showOnlyClassroomActivities ? eq(activities.classroomId, classroom.id) : undefined,
+                // TODO migration classroom && classroom.showOnlyClassroomActivities ? eq(activities.classroomId, classroom.id) : undefined,
                 isNotNull(activities.publishDate),
                 isNull(activities.deleteDate),
                 search !== null
