2cb546f69b04e6726d2af3e64fe84632ec3336c2
stephane-blanchon-viseo <stephane.blanchon@viseo.com>
2026-04-17 18:26:53 +0200

    VIL-979: use new <BaseTemplate> component for in RequestNewPasswordTemplate.tsx

--- src/server/emails/templates/RequestNewPasswordTemplate.tsx
+++ src/server/emails/templates/RequestNewPasswordTemplate.tsx
@@ -1,7 +1,7 @@
-import { Html, Container, Section, Heading, Img, Link, Text, Font } from '@react-email/components';
+import { Section, Heading, Link, Text } from '@react-email/components';
 import type { BaseTemplateProps } from '@server/emails/templates/BaseTemplate';
-import styles from '@server/emails/templates/utils/emailStyles';
-import { getEnvVariable } from '@server/lib/get-env-variable';
+import BaseTemplate from '@server/emails/templates/BaseTemplate';
+// import styles from '@server/emails/templates/utils/emailStyles';
 import { getExtracted } from 'next-intl/server';
 
 import type { RequestNewPasswordTemplateData } from './utils/types';
@@ -10,8 +10,7 @@ interface RequestNewPasswordTemplateProps extends BaseTemplateProps, RequestNewP
     translations: {
         initializeNewPassword: string;
         followLinktoResetPassword: string;
-        goTo1Village: string;
-        followUs: string;
+        resetMyPassword: string;
     };
 }
 
@@ -20,54 +19,20 @@ export default function RequestNewPasswordTemplate({
     translations,
     baseTranslations,
 }: RequestNewPasswordTemplateProps) {
-    const { initializeNewPassword, followLinktoResetPassword, goTo1Village, followUs } = translations;
-    const { altText, notification } = baseTranslations;
-    const baseUrl = getEnvVariable('HOST_URL');
+    const { initializeNewPassword, followLinktoResetPassword, resetMyPassword } = translations;
 
     return (
-        <Html>
-            <Font fontFamily="Roboto" fallbackFontFamily="Arial" fontWeight={400} fontStyle="normal" />
-            <Container style={{ padding: '16px', backgroundColor: styles.primaryColor }}>
-                <Container style={{ textAlign: 'center', backgroundColor: styles.backgroundColor }}>
-                    <Section>
-                        <Img src={`${baseUrl}/static/images/plm-logo-allonge.jpg`} width="100%" height="auto" alt={altText} />
-                        <Heading as="h2" style={{ margin: '16px 0' }}>
-                            {initializeNewPassword}
-                        </Heading>
-                    </Section>
-                    <Section style={{ textAlign: 'left', padding: '16px' }}>
-                        <Text style={{ margin: '0' }}>{followLinktoResetPassword}</Text>
-                        <Link style={{ fontSize: '14px' }} href={resetPasswordLink}>
-                            {resetPasswordLink}
-                        </Link>
-                    </Section>
-                    <Section style={{ padding: '16px' }}>
-                        <Link
-                            href={baseUrl}
-                            target="_blank"
-                            style={{ ...styles.button, borderColor: styles.primaryColor, color: styles.primaryColor }}
-                        >
-                            <Img src={`${baseUrl}/static/images/android-chrome-192x192.png`} alt="1Village" width={24} height={24} /> {goTo1Village}
-                        </Link>
-                    </Section>
-                    <Section>
-                        <Text style={{ fontSize: '12px', fontStyle: 'italic', color: styles.grey500 }}>{notification}</Text>
-                    </Section>
-                    <Section style={{ padding: '0 0 16px' }}>
-                        <Text style={{ fontSize: '16px', fontWeight: 'bold', textTransform: 'uppercase', marginBottom: '8px' }}>{followUs}</Text>
-                        <Link href="https://www.facebook.com/associationparlemonde" style={{ ...styles.socialButton, margin: '0 4px' }}>
-                            <Img src={`${baseUrl}/static/svg/icon-facebook.svg`} alt="Facebook" width={18} height={18} />
-                        </Link>
-                        <Link href="https://www.linkedin.com/company/par-le-monde/" style={{ ...styles.socialButton, margin: '0 4px' }}>
-                            <Img src={`${baseUrl}/static/svg/icon-linkedin.svg`} alt="LinkedIn" width={18} height={18} />
-                        </Link>
-                        <Link href="https://www.youtube.com/@parlemonde6324" style={{ ...styles.socialButton, margin: '0 4px' }}>
-                            <Img src={`${baseUrl}/static/svg/icon-youtube.svg`} alt="Youtube" width={18} height={18} />
-                        </Link>
-                    </Section>
-                </Container>
-            </Container>
-        </Html>
+        <BaseTemplate baseTranslations={baseTranslations}>
+            <Heading as="h2" style={{ margin: '16px 0' }}>
+                {initializeNewPassword}
+            </Heading>
+            <Section style={{ textAlign: 'left' }}>
+                <Text style={{ margin: '0' }}>{followLinktoResetPassword}</Text>
+                <Link style={{ fontSize: '14px' }} href={resetPasswordLink}>
+                    {resetMyPassword}
+                </Link>
+            </Section>
+        </BaseTemplate>
     );
 }
 
@@ -84,8 +49,7 @@ RequestNewPasswordTemplate.PreviewProps = {
     translations: {
         initializeNewPassword: 'Réinitialisation de votre mot de passe',
         followLinktoResetPassword: 'En suivant le lien ci-dessous, vous pourrez modifier votre mot de passe :',
-        goTo1Village: 'Allez sur 1Village',
-        followUs: 'Suivez-nous !',
+        resetMyPassword: 'Modifier mon mot de passe',
     },
 } as const;
 
@@ -95,8 +59,7 @@ export const getRequestNewPasswordTranslations = async () => {
     const translations: RequestNewPasswordTemplateProps['translations'] = {
         initializeNewPassword: t('Réinitialisation de votre mot de passe'),
         followLinktoResetPassword: t('En suivant le lien ci-dessous, vous pourrez modifier votre mot de passe :'),
-        goTo1Village: t('Allez sur 1Village'),
-        followUs: t('Suivez-nous !'),
+        resetMyPassword: t('Modifier mon mot de passe'),
     };
 
     return translations;
