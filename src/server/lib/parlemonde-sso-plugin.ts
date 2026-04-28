b983871d5a9d4eb918b1f5df9bfb3bdae9a3768f
Serge Arnone <serge.arnone@orange.com>
2026-04-28 11:06:21 +0200

    VIL-932: added SSO_BASE_URL variable

--- src/server/lib/parlemonde-sso-plugin.ts
+++ src/server/lib/parlemonde-sso-plugin.ts
@@ -46,12 +46,15 @@ export const ssoPlugin = registerService('parlemonde-sso-plugin', () =>
                       responseType: 'code',
                       clientId: CLIENT_ID,
                       clientSecret: CLIENT_SECRET,
-                      authorizationUrl: `https://prof.parlemonde.org/oauth/authorize`,
-                      tokenUrl: `https://prof.parlemonde.org/oauth/token`,
+                      authorizationUrl: `${getEnvVariable('SSO_BASE_URL')}/oauth/authorize`,
+                      tokenUrl: `${getEnvVariable('SSO_BASE_URL')}/oauth/token`,
                       getUserInfo: async (tokens) => {
-                          const plmUser = await jsonFetcher<PLMUser>(`https://prof.parlemonde.org/oauth/me?access_token=${tokens.accessToken}`, {
-                              method: 'GET',
-                          });
+                          const plmUser = await jsonFetcher<PLMUser>(
+                              `${getEnvVariable('SSO_BASE_URL')}/oauth/me?access_token=${tokens.accessToken}`,
+                              {
+                                  method: 'GET',
+                              },
+                          );
                           // 1. create user in database if not exists
                           let user: { id: string; role: UserRole } | undefined = await db.query.users.findFirst({
                               columns: {
