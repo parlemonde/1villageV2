5d8e263256a9852dc486faa7a479fce4f3659ea3
stephane-blanchon-viseo <stephane.blanchon@viseo.com>
2026-05-21 11:07:18 +0200

    VIL-968: delete previous migrations

--- drizzle/0012_puzzling_warbird.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "comments" ADD COLUMN "classroomId" integer;--> statement-breakpoint
-ALTER TABLE "comments" ADD CONSTRAINT "comments_classroomId_classrooms_id_fk" FOREIGN KEY ("classroomId") REFERENCES "public"."classrooms"("id") ON DELETE cascade ON UPDATE no action;
