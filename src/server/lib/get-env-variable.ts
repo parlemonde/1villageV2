1175e81374a859ea47fb3a1a1206cfbf8c518666
Serge Arnone <serge.arnone@orange.com>
2026-05-27 18:01:41 +0200

    VIL-932: SSO config, redirectURI and authentication fixes

--- src/server/lib/get-env-variable.ts
+++ src/server/lib/get-env-variable.ts
@@ -11,6 +11,7 @@ const DEFAULT_ENV_VARIABLES = {
     DATABASE_URL: 'postgresql://postgres:example@localhost:5432/un_village',
     CLIENT_ID: '',
     CLIENT_SECRET: '',
+    SSO_BASE_URL: 'https://professeur.parlemonde.org',
     AWS_ACCESS_KEY_ID: 'local',
     AWS_SECRET_ACCESS_KEY: 'local',
     AWS_SESSION_TOKEN: '',
