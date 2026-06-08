bc734d867ad8835576ddf03a5a3f1340b4413367
stephane-blanchon-viseo <stephane.blanchon@viseo.com>
2026-06-08 09:03:25 +0200

    VIL-1000: reset file get-current-user.ts with previous version, i.e without any 'use cache' nor DB select()

--- src/server/helpers/get-current-user.ts
+++ src/server/helpers/get-current-user.ts
@@ -1,37 +1,10 @@
 import { trace } from '@opentelemetry/api';
-import { db } from '@server/database';
 import type { User } from '@server/database/schemas/users';
-import { users } from '@server/database/schemas/users';
 import { auth } from '@server/lib/auth';
-import { eq } from 'drizzle-orm';
-import { cacheTag, updateTag } from 'next/cache';
 import { headers } from 'next/headers';
-import { connection } from 'next/server';
 import { cache } from 'react';
 
 const tracer = trace.getTracer('auth');
-
-const getUserExtraDataImpl = async (userId: string) => {
-    'use cache';
-    cacheTag('userExtraData');
-    return await db
-        .select({
-            firstLogin: users.firstLogin,
-            adminPublicationSubscribed: users.adminPublicationSubscribed,
-            commentActivitySubscribed: users.commentActivitySubscribed,
-        })
-        .from(users)
-        .where(eq(users.id, userId));
-};
-
-export async function getUserExtraData(userId: string) {
-    await connection();
-    return getUserExtraDataImpl(userId);
-}
-
-export async function invalidateUserExtraData() {
-    updateTag('userExtraData');
-}
 
 export const getCurrentUser = cache(async (): Promise<User | undefined> => {
     return tracer.startActiveSpan('getCurrentUser', async (span) => {
@@ -39,14 +12,7 @@ export const getCurrentUser = cache(async (): Promise<User | undefined> => {
             const session = await auth.api.getSession({
                 headers: await headers(),
             });
-            let user = session?.user as User | undefined;
-
-            if (user) {
-                const extraData = await getUserExtraData(user.id);
-                if (extraData && extraData.length > 0) {
-                    user = { ...user, ...extraData[0] };
-                }
-            }
+            const user = session?.user as User | undefined;
 
             if (user) {
                 span.setAttributes({
